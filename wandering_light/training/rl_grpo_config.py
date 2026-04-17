35ef32fd1c6ee21394143790150ab65d70d931a5
Abhishek Rao <abhishekrao5002@gmail.com>
2026-04-17 16:35:16 -0700

    Set default length penalty to 0 in RL training.

--- wandering_light/training/rl_grpo_config.py
+++ wandering_light/training/rl_grpo_config.py
@@ -23,7 +23,9 @@ class RLGRPOConfig:
     DEFAULT_LEARNING_RATE: float = 1e-6
     DEFAULT_MAX_LENGTH: int = 256
     DEFAULT_NUM_EPOCHS: int = 3
-    DEFAULT_LENGTH_PENALTY: float = 0.1
+    DEFAULT_LENGTH_PENALTY: float = (
+        0.0  # TODO: Investigate why higher value reduces accuracy
+    )
     DEFAULT_SOLVER_ATTEMPTS: int = 8
     INFERENCE_BATCH_SIZE: int = 64
     TRAINING_INTERVAL_STEPS: int = 8
